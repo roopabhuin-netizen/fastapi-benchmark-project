7931d2d85bb8dad2cd79df2159abe621af84edbe
Roopa Bhuin <roopabhuin@gmail.com>
2026-02-27 16:03:01 +0530

    added indexing in database

--- app/database.py
+++ app/database.py
@@ -3,3 +3,7 @@ from app.config import settings
 
 client = MongoClient(settings.mongodb_url)
 db = client[settings.database_name]
+
+db["executions"].create_index("benchmark_id")
+db["executions"].create_index("executed_at")
+db["metrics"].create_index("execution_id")
